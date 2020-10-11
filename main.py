61c28eb094e501629dcc7000839612c476868baa
flamegun <70954932+flamegun@users.noreply.github.com>
2020-10-11 13:44:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ file_handle2 = open('Answers.txt','w+')
 #file_handle = open('Exercises.txt','w+')
 file_handle1.close( )
 file_handle2.close( )
+
 subject_num = int(input('需要生成的题目个数：'))
 scope = int(input('需要生成四则运算的数最大为：'))
+
 for i in range(0,subject_num):
     num1 = random.randint(1,scope)
     num2 = random.randint(1,scope) 
