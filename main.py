b0a376b6546f43eaae92dbd2a37619c4135ba8f6
flamegun <70954932+flamegun@users.noreply.github.com>
2020-10-12 20:14:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ for i in range(0,subject_num):
         expression = str(num1) + symbol[n1] + str(num2) + symbol[n2] + str(num3)
 
 
-    file_handle1 = open('Exercises.txt','a')#写入题目
+    file_handle1 = open('Exercises.txt','a')  #写入题目
     file_handle1.writelines( '四则运算题目.' + str(i) +  '\t'  + expression + '=' + '\n')
     file_handle1.close()
 
@@ -34,12 +34,12 @@ for i in range(0,subject_num):
     result = Fraction(result).limit_denominator()  #结果转化为近似分数
     w = result.numerator
     r = result.denominator
-    if w > r and r != 1:#判断是否为带分数
+    if w > r and r != 1: #判断是否为带分数
         result_end = str(str(int(w / r)) + "'" + str(w % r) + "/" + str(r))#转化为带分数形式
     else:
         result_end = str(result)
 
-    file_handle2 = open('Answers.txt','a')#写入答案
+    file_handle2 = open('Answers.txt','a') #写入答案
     file_handle2.writelines('第' + str(i) + '题的答案：' + '\t' + result_end + '\n')
     file_handle1.close()
 
